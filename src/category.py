ab0e4769cff25b35065b26fd28b25491168580ce
brian-burdine <brian.burdine@student.awesomeinc.org>
2023-03-16 19:43:49 +0000

    Wrote deposit method and tests

--- src/category.py
+++ src/category.py
@@ -2,3 +2,13 @@ class Category():
     def __init__(self, name):
         self.name = name
         self.ledger = []
+    
+    def deposit(self, amount, *description):
+        if description:
+            self.ledger.append(
+                {"amount": amount, "description": description}
+            )
+        else:
+            self.ledger.append(
+                {"amount": amount, "description": ""}
+            )
